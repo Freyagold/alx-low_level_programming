2ee88e5243db709f04e9156bf2e189d373f2f5c5
Idehen Godsgift <godsgiftidehen@gmail.com>
2022-09-09 14:27:39 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: freya
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int freya;
+
+	for (freya = 48; freya < 58; freya++)
+		putchar(freya);
+	putchar('\n');
+	return (0);
+}
