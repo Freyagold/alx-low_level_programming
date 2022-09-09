7aff39435185c2bf997106eaf53fd19f7257256c
Idehen Godsgift <godsgiftidehen@gmail.com>
2022-09-09 13:48:08 -0700

    5prints

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: freya
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int freya;
+
+	for (freya = 0; freya < 10; freya++)
+	{
+		printf("%d",freya);
+	}
+	printf("\n");
+	return (0);
+}
