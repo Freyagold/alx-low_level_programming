93843a65b7909ba309c4983238f616a51d1f50ac
Idehen Godsgift <godsgiftidehen@gmail.com>
2022-09-09 15:25:34 -0700

    printbase16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,18 +2,23 @@
 
 /**
  * main - Entry point
- * autor: freya
- * Return: Always 0 (Success)
+ *
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
-	int freya;
-	char gold;
+	int i;
+	char j;
 
-	for (freya = 48; freya < 58; freya++)
-		putchar(sammy);
-	for (gold = 'a'; gold <= 'f'; gold++)
-		putchar(gold);
+	for (i = 48; i < 58; i++)
+	{
+
+		putchar(i);
+	}
+	for (j = 'a'; j <= 'f'; j++)
+	{
+		putchar(j);
+	}
 	putchar('\n');
 	return (0);
 }
